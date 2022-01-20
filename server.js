82798fb0ef0ef76dddd41c208399ca3baf84e13c
unknown <codybonsma@gmail.com>
2022-01-20 15:07:14 -0500

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5280;
+const cors = require('cors');
+
+app.use(cors());
 
 //middelware
 app.use(express.json());
