7d448194d0d43cfb9cfea94e8d7db9e1d9aab177
unknown <codybonsma@gmail.com>
2022-01-20 14:27:41 -0500

    index mods

--- models/index.js
+++ models/index.js
@@ -6,14 +6,15 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 // construct the sequelize object using the constructor
 let sequelize = null;
-
     if (process && process.env.DATABASE_URL) {
         sequelize = new Sequelize(process.env.DATABASE_URL, {
+            dialect: 'postgres',
+            protocol: 'postgres',
             dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-                }
+              ssl: {
+                  require: true,
+                  rejectUnauthorized: false
+                  }
               }
             }
         );
@@ -25,6 +26,12 @@ let sequelize = null;
             password: dbConfig.PASSWORD,
             dialect: dbConfig.dialect,
             host: dbConfig.HOST,
+            dialectOptions: {
+              ssl: {
+                require: true,
+                rejectUnauthorized: false
+              }
+            }
         })
     }
 
