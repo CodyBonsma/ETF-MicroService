7d4d8814562554338a981fe02eaf713d169e1c95
unknown <codybonsma@gmail.com>
2022-02-06 17:15:39 -0500

    edits

--- controllers/etfController.js
+++ controllers/etfController.js
@@ -1,44 +1,44 @@
 // require the db created in index file
-const db = require('../models/index');
+const db = require("../models/index");
 
 // get the ETF model
 const ETF = db.ETF;
 
 const addETF = async (req, res) => {
-    let input_data = {
-        name: req.body.name,
-        ticker: req.body.ticker,
-        price: req.body.price,
-        assetClass: req.body.assetClass,
-        expenseRatio: req.body.expenseRatio,
-        change: req.body.change,
-        SEC_yield: req.body.SEC_yield,
-        YTD: req.body.YTD,
-        oneYear: req.body.oneYear,
-        fiveYear: req.body.fiveYear,
-        tenYear: req.body.tenYear,
-        sinceInception: req.body.sinceInception
-    };
+  let input_data = {
+    name: req.body.name,
+    ticker: req.body.ticker,
+    price: req.body.price,
+    assetClass: req.body.assetClass,
+    expenseRatio: req.body.expenseRatio,
+    change: req.body.change,
+    SEC_yield: req.body.SEC_yield,
+    YTD: req.body.YTD,
+    oneYear: req.body.oneYear,
+    fiveYear: req.body.fiveYear,
+    tenYear: req.body.tenYear,
+    sinceInception: req.body.sinceInception,
+  };
 
-    const etf = await ETF.create(input_data);
-    res.status(200).send(etf);
+  const etf = await ETF.create(input_data);
+  res.status(200).send(etf);
 };
 
 const getAllETFs = async (req, res) => {
-    let etfs = await ETF.findAll({include: db.Holdings});
-    res.status(200).send(etfs);
+  let etfs = await ETF.findAll();
+  res.status(200).send(etfs);
 };
 
 const getOneEtf = async (req, res) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    let etf = await ETF.findOne({where: {id: id}});
-    res.status(200).send(etf)
+  let etf = await ETF.findOne({ where: { id: id } });
+  res.status(200).send(etf);
 };
 
 // export all the controller functions
 module.exports = {
-    addETF,
-    getAllETFs,
-    getOneEtf
-}
+  addETF,
+  getAllETFs,
+  getOneEtf,
+};
